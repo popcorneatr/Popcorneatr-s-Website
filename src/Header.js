61be9b78e3e1e888f5589349f59b88f02bc19b26
Austin Nguyen <106615117+popcorneatr@users.noreply.github.com>
2023-06-13 10:14:14 -0700

    header navbar design complete

--- src/Header.js
+++ src/Header.js
@@ -1,5 +1,7 @@
 import React from "react";
 import "./Header.css";
+import SearchIcon from "@mui/icons-material/Search";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 
 function Header() {
   return (
@@ -13,23 +15,34 @@ function Header() {
       <h1 className="store_name">Popcorneatr's Card Corner</h1>
 
       <div className="header_search">
-        <input className="search_field" type="text" />
+        <input
+          className="header_search_field"
+          type="text"
+          placeholder="Search..."
+          title="search bar"
+        />
+        <SearchIcon className="header_search_icon" />
       </div>
 
       <div className="header_nav">
         <div className="header_option">
           <span className="header_option_line_one">Hello Guest</span>
-          <span className="two">Sign In</span>
+          <span className="header_option_line_two">Sign In</span>
         </div>
 
         <div className="header_option">
           <span className="header_option_line_one">Returns</span>
-          <span className="two">& Orders</span>
+          <span className="header_option_line_two">& Orders</span>
         </div>
 
         <div className="header_option">
           <span className="header_option_line_one">Your</span>
-          <span className="two">Prime</span>
+          <span className="header_option_line_two">Prime</span>
+        </div>
+
+        <div className="header_option_cart_icon">
+          <ShoppingCartIcon />
+          <span className="header_option_line_two header_cart_count">0</span>
         </div>
       </div>
     </div>
